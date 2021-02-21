6c72d21f765774a974affefc98dce11c87b5f455
254621340@qq.com <Wsf55581f>
2021-02-21 17:16:53 +0800

    添加gateway 聚合文档

---
+++ auth/src/main/java/com/pch/auth/config/SwaggerConfiguration.java
@@ -0,0 +1,54 @@
+package com.pch.auth.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.core.annotation.Order;
+
+import com.github.xiaoymin.knife4j.spring.annotations.EnableSwaggerBootstrapUi;
+
+import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * <P> swagger 文档配置类 </P>
+ *
+ * @Author: pch
+ * @Date: 2021/2/21
+ */
+@Configuration
+@EnableSwagger2
+@EnableSwaggerBootstrapUi
+@Import(BeanValidatorPluginsConfiguration.class)
+public class SwaggerConfiguration {
+
+    @Bean(value = "userApi")
+    @Order(value = 1)
+    public Docket groupRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(groupApiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.pch.auth.rest"))
+                .paths(PathSelectors.any())
+
+                .build();
+    }
+
+    private ApiInfo groupApiInfo(){
+        return new ApiInfoBuilder()
+                .title("swagger-bootstrap-ui很棒~~~！！！")
+                .description("<div style='font-size:14px;color:red;'>swagger-bootstrap-ui-demo RESTful APIs</div>")
+                .termsOfServiceUrl("http://www.group.com/")
+                .contact("group@qq.com")
+                .version("1.0")
+                .build();
+    }
+
+
+}
--- gateway/gateway-admin/src/main/java/com/pch/gateway/config/SwaggerResourceConfig.java
+++ gateway/gateway-admin/src/main/java/com/pch/gateway/config/SwaggerResourceConfig.java
@@ -12,13 +12,16 @@ import java.util.List;
 import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.gateway.config.GatewayProperties;
 import org.springframework.cloud.gateway.route.RouteDefinition;
+import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.support.NameUtils;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 import com.pch.gateway.router.RedisRouteDefinitionRepository;
 
+import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import springfox.documentation.swagger.web.SwaggerResource;
 import springfox.documentation.swagger.web.SwaggerResourcesProvider;
@@ -26,10 +29,10 @@ import springfox.documentation.swagger.web.SwaggerResourcesProvider;
 @Slf4j
 @Component
 @Primary
+@AllArgsConstructor
 public class SwaggerResourceConfig implements SwaggerResourcesProvider {
 
-    @Autowired(required = false)
-    private RedisRouteDefinitionRepository routeDefinitionRepository;
+    private final RedisRouteDefinitionRepository routeDefinitionRepository;
 
     @Override
     public List<SwaggerResource> get() {
@@ -39,8 +42,7 @@ public class SwaggerResourceConfig implements SwaggerResourcesProvider {
         routeDefinitions.forEach(route -> route.getPredicates().stream()
                 .filter(predicateDefinition -> ("Path").equalsIgnoreCase(predicateDefinition.getName()))
                 .forEach(predicateDefinition -> resources.add(swaggerResource(route.getId(),
-                        predicateDefinition.getArgs().get(NameUtils.GENERATED_NAME_PREFIX + "0")
-                                .replace("**", "v2/api-docs")))));
+                        predicateDefinition.getArgs().get("pattern").replace("**", "v2/api-docs")))));
         return resources;
     }
 
