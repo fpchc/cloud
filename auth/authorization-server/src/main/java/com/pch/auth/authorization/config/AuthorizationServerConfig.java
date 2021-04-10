20383bd6d781e2e4d35f1342af4b4aadbe068561
254621340@qq.com <Wsf55581f>
2021-04-10 11:31:33 +0800

    更改编码格式

--- auth/authorization-server/src/main/java/com/pch/auth/authorization/config/AuthorizationServerConfig.java
+++ auth/authorization-server/src/main/java/com/pch/auth/authorization/config/AuthorizationServerConfig.java
@@ -1,11 +1,11 @@
 package com.pch.auth.authorization.config;
 
-import java.math.BigDecimal;
 import java.util.Arrays;
 import java.util.List;
 
 import javax.sql.DataSource;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
